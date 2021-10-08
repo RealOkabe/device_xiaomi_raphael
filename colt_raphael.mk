5954bbc8b99c23be41a72f0b563fa1cdf0407931
MadScientistA <anmolzagrawal@gmail.com>
2021-10-08 21:37:24 +0200

    Adapt tree for colt-enigma
    1. Inherit colt vendor stuff
    2. Added Boot Animation res
    3. Added colt maintainer name

---
+++ colt_raphael.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from raphael device
+$(call inherit-product, device/xiaomi/raphael/device.mk)
+
+# Inherit some common ColtOS stuff.
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := colt_raphael
+PRODUCT_DEVICE := raphael
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi K20 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+COLT_BUILD_MAINTAINER := RealOkabe
+
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
+
+TARGET_INCLUDE_PIXEL_CHARGER := true
